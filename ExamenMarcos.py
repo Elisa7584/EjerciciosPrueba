a7137ea55b0661e2910164e9ef3366c818aef4f0
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2023-11-17 10:16:54 +0100

    Update ExamenMarcos.py

--- ExamenMarcos.py
+++ ExamenMarcos.py
@@ -1,5 +1,26 @@
+def longitud(lista):
+    longitud = 0
+    for elemento in lista:
+        longitud += 1
+    return longitud
+
+def buscarLetra(lista,letra,num):
+    letraEncontrada = "no encontrada"
+    for pos in range(longitud(lista)):
+        if lista[pos] == letra:
+            letraEncontrada = lista[pos+num]
+    return letraEncontrada
+    
+# Primero creamos la lista con las letras necesarias
 letras = "abcdefghijklmnñopqrstuvwxyz"
 listaLetras = []
-# EJERCICIO 1 - Primero creamos la lista con las letras necesarias
-for letra in letras:    
-    listaLetras.append(letra)
+
+for l in letras:    
+    listaLetras.append(l)
+    
+# Pedimos datos al usuario
+
+letra = input("Introduce una letra -> ")
+num = int(input("Introduce un numero -> "))
+l = buscarLetra(listaLetras,letra,num)
+print(l)
