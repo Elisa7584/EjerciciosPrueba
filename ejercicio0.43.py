97bffb16f69ae3095f91550e63f20acb16dac7a5
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2023-11-17 09:17:00 +0100

    Update ejercicio0.43.py

--- ejercicio0.43.py
+++ ejercicio0.43.py
@@ -4,4 +4,4 @@ letraUsuario = input("Introduce una vocal: ")
 
 while letraUsuario == "a" or letraUsuario == "e" or letraUsuario == "i" or letraUsuario == "o" or letraUsuario == "u":
     letraUsuario = input("Lo siento, no es una consonante , vuelve a introducirla: ")
-print("Es una consonante")
+print(f"La consonante es {letraUsuario} consonante")
