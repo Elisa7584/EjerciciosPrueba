e47059748976f0bc483400da06794846da69c34d
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2023-12-01 08:31:49 +0100

    Update ExamenMarcos.py

--- ExamenMarcos.py
+++ ExamenMarcos.py
@@ -1,1 +1,11 @@
-hola elisa
+# Función para crear una matriz de 10x10 rellena de los números del 0 al 99
+def crear_matriz():
+    matriz = []
+    num = 0
+    for i in range(10):
+        fila = []
+        for j in range(10):
+            fila.append(num)
+            num += 1
+        matriz.append(fila)
+    return matriz
